e24d49b65282284dfdb953401d7d0f8082f4df75
agrrs <sofihurtova@gmail.com>
2020-06-02 14:47:25 +0300

    Add task0202

---
+++ src/main/java/ua/edu/npu/lab02/task02/Application.java
@@ -0,0 +1,10 @@
+package ua.edu.npu.lab02.task02;
+
+public class Application {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Charlie", 4);
+        Dog dog2 = new Dog("Oscar", 2);
+        dog1.voice();
+        dog2.voice();
+    }
+}
